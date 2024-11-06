095d547522850204fccda3d44fea7a8e402cfceb
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2024-11-07 00:02:45 +0530

    updated course route

--- src/Components/CourseForm.js
+++ src/Components/CourseForm.js
@@ -46,7 +46,7 @@ const CourseForm = () => {
         };
     
         try {
-            const response = await fetch('http://localhost:1337/api/courses', {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/coursesr`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
